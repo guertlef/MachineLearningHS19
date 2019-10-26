7d84172ed6830330a152a5fb50341b375f78ff83
guertlef <fabienne.guertler@students.unibe.ch>
2019-10-26 10:16:47 +0200

    Add Solutions to Assignments 01-03.

---
+++ ProgrammingAssignments/U1/assignment1_solutions/tanh.py
@@ -0,0 +1,27 @@
+from numpy import exp
+
+
+def tanh(z):
+    """
+    Computes the tanh of z element-wise.
+
+    Args:
+        z: An np.array of arbitrary shape
+
+    Returns:
+        g: An np.array of the same shape as z
+
+    """
+
+    g = None
+    #######################################################################
+    # TODO:                                                               #
+    # Compute and return the tanh of z in g                            #
+    #######################################################################
+
+    pass
+
+    #######################################################################
+    #                         END OF YOUR CODE                            #
+    #######################################################################
+    return g
